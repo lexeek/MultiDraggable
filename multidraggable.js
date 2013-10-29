1a801a36ab49be8475f08eae45c2cc4b642211b4
lexeek <lexeek@gmail.com>
2013-10-29 20:32:23 +0300

    Update multidraggable.js

--- multidraggable.js
+++ multidraggable.js
@@ -1,49 +1,66 @@
 /**
- * JQuery MultiDraggable Plugin
+ * Written by Tikhonov Alexey MultiDraggable for jsPlumb nodes
+ *
+ * Fork of JQuery MultiDraggable Plugin  (https://github.com/someshwara/MultiDraggable)
  *
  * Licensed under the MIT (http://www.opensource.org/licenses/mit-license.php)
- *
- * Written by Sudheer Someshwara <sudheer.someshwara@gmail.com>
- *
- * MultiDraggable is a jQuery plugin which extends jQuery UI Draggable to add multi drag and live functionality.
- *
-**/
+ **/
+
 (function ($, undefined) {
-   $.fn.multiDraggable = function (opts) {
-   	var initLeftOffset = []
-   	    ,initTopOffset = [];	    
-   	return this.each (function (){
-   		 $(this).live("mouseover", function() {
-		         if (!$(this).data("init")) {
-		            $(this).data("init", true).draggable(opts,{
-				         start: function (event,ui) {
-							var pos = $(this).position();
-							$.each(opts.group || {}, function(key,value) {
-								var elemPos = $(value).position();
-								initLeftOffset[key] = elemPos.left - pos.left;
-								initTopOffset[key] = elemPos.top - pos.top;			
-							});		
-							opts.startNative ? opts.startNative() : {};
-						},
-						drag: function(event,ui) {
-							var pos = $(this).offset();
-							$.each(opts.group || {}, function(key,value) {
-								$(value).offset({left: pos.left + initLeftOffset[key], 
-								top: pos.top + initTopOffset[key]});				
-							});
-							opts.dragNative ? opts.dragNative() : {};
-						},
-						stop: function(event,ui) {
-							var pos = $(this).offset();
-							$.each(opts.group || {}, function(key,value) {
-								$(value).offset({left: pos.left + initLeftOffset[key], 
-								top: pos.top + initTopOffset[key]});								
-							});
-							opts.stopNative ? opts.stopNative() : {};
-						},
-					});
-		        }
-	      });
- 	 });
-   };
- }(jQuery));
+  $.fn.multiDraggable = function (opts) {
+    var initLeftOffset = []
+      , initTopOffset = [];
+    return this.each(function (n, el) {
+      el = '#'+el;
+      if (!$(el).data("init")) {
+        $(el).data("init", true).draggable(opts, {
+          start: function (event, ui) {
+            var item = $(el);
+            var pos = item.position();
+            var items = $(opts.group);
+            $.each(items || {}, function (key, value) {
+              var elemPos = $('#' + value).position();
+              initLeftOffset[key] = elemPos.left - pos.left;
+              initTopOffset[key] = elemPos.top - pos.top;
+            });
+            opts.startNative ? opts.startNative() : {};
+            jsPlumb.repaint(items);
+            jsPlumb.repaint(item);
+          },
+          drag: function (event, ui) {
+            var item = $(el);
+            var pos = ui.offset;
+            var items = $(opts.group);
+            $.each(items || {}, function (key, value) {
+
+              var oPos = {
+                left: pos.left + initLeftOffset[key],
+                top: pos.top + initTopOffset[key]
+              }, oEl = $('#' + value);
+
+              oEl.offset(oPos);
+              jsPlumb.repaint(oEl, oPos);
+            });
+
+            jsPlumb.repaint(item, pos);
+            opts.dragNative ? opts.dragNative() : {};
+          },
+          stop: function (event, ui) {
+            var item = $(el);
+            var pos = $(el).offset();
+            var items = $(opts.group);
+            //         console.log("stop",ui,event)
+            $.each(items || {}, function (key, value) {
+              $('#' + value).offset({
+                left: pos.left + initLeftOffset[key],
+                top: pos.top + initTopOffset[key]
+              });
+            });
+            opts.stopNative ? opts.stopNative() : {};
+            jsPlumb.repaint(items);
+          }
+        });
+      }
+    });
+  };
+}(jQuery));
